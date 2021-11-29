41fe7b7cee8cb1118f75b00aa8f883e05c6bcb1b
Sam <supersonischsam@gmail.com>
2021-11-29 01:14:48 +0100

    Added sketch

---
+++ esp8266/image/image.ino
@@ -0,0 +1,129 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com
+*********/
+
+// Load Wi-Fi library
+#include <ESP8266WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "SSID";
+const char* password = "Pass";
+
+WiFiServer server(80);
+
+
+// Decode HTTP GET value
+String redString = "0";
+String greenString = "0";
+String blueString = "0";
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int pos4 = 0;
+
+// Variable to store the HTTP req  uest
+String header;
+
+// Red, green, and blue pins for PWM control
+const int redPin = 13;     // 13 corresponds to GPIO13
+const int greenPin = 12;   // 12 corresponds to GPIO12
+const int bluePin = 14;    // 14 corresponds to GPIO14
+
+// Setting PWM bit resolution
+const int resolution = 256;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+
+  // configure LED PWM resolution/range and set pins to LOW
+  analogWriteRange(resolution);
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 0);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.hostname("EspLightController");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
+}
+
+void loop() {
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {            // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println("Connection: keep-alive");
+            client.println();
+
+            // Request sample: /?r201g32b255&
+            // Red = 201 | Green = 32 | Blue = 255
+            if (header.indexOf("GET /?r") >= 0 || header.indexOf("HEAD /?r") >= 0) {
+              pos1 = header.indexOf('r');
+              pos2 = header.indexOf('g');
+              pos3 = header.indexOf('b');
+              pos4 = header.indexOf('&');
+              redString = header.substring(pos1 + 1, pos2);
+              greenString = header.substring(pos2 + 1, pos3);
+              blueString = header.substring(pos3 + 1, pos4);
+              /*Serial.println(redString.toInt());
+                Serial.println(greenString.toInt());
+                Serial.println(blueString.toInt());*/
+              analogWrite(redPin, redString.toInt());
+              analogWrite(greenPin, greenString.toInt());
+              analogWrite(bluePin, blueString.toInt());
+            }
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
